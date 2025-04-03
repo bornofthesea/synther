5251ca51161c97bf112aa4c5a3321f5d81489224
Morgan <cmorgan@usp.br>
2025-04-03 09:27:01 -0300

    Some small changes in line plotting

--- fits2txt/.ipynb_checkpoints/createList-checkpoint.py
+++ fits2txt/.ipynb_checkpoints/createList-checkpoint.py
@@ -5,10 +5,12 @@ from astropy.io import fits
 hp1dir = '/home/morgan/PhD/Data/spectra/hp1/'
 smcdir = '/home/morgan/PhD/Data/spectra/smc/'
 n6558dir = '/home/morgan/PhD/Data/spectra/n6558/'
+n6522dir = '/home/morgan/PhD/Data/spectra/n6522/'
 
 hp1list = open(hp1dir + 'hp1.lis', 'r')
 smclist = open(smcdir + 'smc.lis', 'r')
 n6558list = open(n6558dir + '6558.lis', 'r')
+n6522list = open(n6522dir + '6522.lis', 'r')
 
 hp1file = open('runhp1.sh', 'w')
 
@@ -33,3 +35,11 @@ for line in n6558list:
     n6558file.write(myString + '\n')
 
 n6558file.close()
+
+n6522file = open('run6522.sh', 'w')
+
+for line in n6522list:
+    myString = 'python fits2txt.py ' + n6522dir + str(line)
+    n6522file.write(myString + '\n')
+
+n6522file.close()
---
+++ fits2txt/run6522.sh
@@ -0,0 +1,12 @@
+python fits2txt.py /home/morgan/PhD/Data/spectra/n6522/apStar-dr17-2M18032356-3001588.fits
+
+python fits2txt.py /home/morgan/PhD/Data/spectra/n6522/apStar-dr17-2M18033660-3002164.fits
+
+python fits2txt.py /home/morgan/PhD/Data/spectra/n6522/apStar-dr17-2M18033819-3000515.fits
+
+python fits2txt.py /home/morgan/PhD/Data/spectra/n6522/apStar-dr17-2M18033965-3000521.fits
+
+python fits2txt.py /home/morgan/PhD/Data/spectra/n6522/apStar-dr17-2M18034052-3003281.fits
+
+python fits2txt.py /home/morgan/PhD/Data/spectra/n6522/asStar-dr17-2M18033965-3000521.fits
+
